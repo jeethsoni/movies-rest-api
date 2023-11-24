92dad358c74fb689db20ab62ab8d7176ce4a97a7
Jeet Soni <jeet.soni0822@gmail.com>
2023-11-24 15:05:50 -0500

    added database health check

--- blueprints/health/blueprint.py
+++ blueprints/health/blueprint.py
@@ -3,6 +3,7 @@ blueprint for health check
 """
 import os
 from flask import Blueprint, jsonify
+from db.db_utils import do_query
 
 
 version = os.getenv("VERSION")
@@ -14,4 +15,14 @@ def health_check():
     """
     a GET handler for api health check
     """
-    return jsonify(message="OK", status=200)
+
+    # executes the sql and returns timestamp
+    sql = "SELECT CURRENT_TIMESTAMP;"
+
+    data = do_query(sql, {})
+
+    # checks the API and database health
+    if data["status"] == 200:
+        return jsonify(api_health="OK", db_health="OK", status=200)
+    else:
+        return jsonify(db_health="NOT OK", status=500)
