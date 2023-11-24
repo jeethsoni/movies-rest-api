43f5a88cbb4dd73f8b46b8a0747c1ff43ed5f294
Jeet Soni <jeet.soni0822@gmail.com>
2023-11-24 11:58:47 -0500

    created Connection class instance

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ Flask app entry point
 
 from flask import Flask
 from blueprints.health.blueprint import health_blueprint
+from db.Connection import Connection
 
 app = Flask(__name__)
+
+conn = Connection()
+app.conn = conn
+
 
 # registers the blueprints
 app.register_blueprint(health_blueprint)
