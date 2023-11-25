e66ed22654e09df7640f5919196adc05e3842684
Jeet Soni <jeet.soni0822@gmail.com>
2023-11-24 22:39:43 -0500

    added comments

--- blueprints/movie/service.py
+++ blueprints/movie/service.py
@@ -2,6 +2,10 @@ from db.db_utils import do_query
 
 
 def svc_get():
+    """
+    GET service to get all records
+    """
+
     sql = "SELECT * FROM movies.movie;"
     result = do_query(sql, {})
 
@@ -9,7 +13,9 @@ def svc_get():
 
 
 def svc_get_by_id(id):
-
+    """
+    GET service to get by ID
+    """
     sql = "SELECT * FROM movies.movie WHERE movie_id = %s;"
     params = [id]
     result = do_query(sql, params)
