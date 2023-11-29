60eb6074fda8c1b108c06c9a2a00db66384a0f61
Jeet Soni <jeet.soni0822@gmail.com>
2023-11-29 16:07:08 -0500

    added POST blueprint and service

--- blueprints/movie/blueprint.py
+++ blueprints/movie/blueprint.py
@@ -2,9 +2,9 @@
 blueprint for movie table
 """
 import os
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
-from blueprints.movie.service import svc_get, svc_get_by_id
+from blueprints.movie.service import svc_get, svc_get_by_id, svc_post
 
 
 version = os.getenv("VERSION")
@@ -29,3 +29,19 @@ def get_by_id(movie_id: int):
     result = svc_get_by_id(movie_id)
 
     return jsonify(status=result["status"], data=result["data"])
+
+
+@movie_blueprint.route("/movie/create", methods=["POST"])
+def post_movie():
+    """
+    A POST handler. Creates a new movie record
+    """
+
+    result = svc_post()
+
+    if result["status"] == 200:
+        status = 201
+    else:
+        status = result["status"]
+
+    return jsonify(status=status)
--- blueprints/movie/service.py
+++ blueprints/movie/service.py
@@ -1,3 +1,4 @@
+from flask import jsonify, request
 from db.db_utils import do_query
 from constants.constants import SCHEMA_NAME, MOVIE
 
@@ -18,6 +19,33 @@ def svc_get_by_id(id):
     """
     sql = f"SELECT * FROM {SCHEMA_NAME}.{MOVIE} WHERE movie_id = %s;"
     params = [id]
+    result = do_query(sql, params)
+
+    return result
+
+
+def svc_post():
+    """
+    POST service
+    """
+
+    # request object
+    payload = request.get_json()
+
+    # parameters for the query
+    title = payload["title"]
+    description = payload["description"]
+    year = payload["movie_year"]
+    rating = payload["rating"]
+    runtime = payload["runtime"]
+    votes = payload["votes"]
+    revenue = payload["revenue"]
+    metascore = payload["metascore"]
+
+    # SQL statement
+    sql = f"INSERT INTO {SCHEMA_NAME}.{MOVIE}(title, description, movie_year, rating, runtime, votes, revenue, metascore) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING *;"
+    params = [title, description, year, rating, runtime, votes, revenue, metascore]
+
     result = do_query(sql, params)
 
     return result
