7725eaf48dec2ab1c387f45a31cd4dd3154c5546
Jeet Soni <jeet.soni0822@gmail.com>
2023-12-29 18:48:21 -0500

    added logger for app

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ from flask import Flask
 from blueprints.health.blueprint import health_blueprint
 from blueprints.movie.blueprint import movie_blueprint
 from db.Connection import Connection
+from logger import logger
 
 
 app = Flask(__name__)
+logger = logger.configure_logger("default", "logs/flask.log")
 
 # pushes the application context
 ctx = app.app_context()
 ctx.push()
+
+app.logger = logger
 
 # creates a Connection instance stores it in app.conn
 conn = Connection()
