a3692e49ffa5b13b5f5f28b44ebe1b43a92d2179
Jeet Soni <jeet.soni0822@gmail.com>
2024-01-06 19:23:15 -0500

    pydantic data validation models

--- blueprints/movie/blueprint.py
+++ blueprints/movie/blueprint.py
@@ -1,8 +1,12 @@
 """
 blueprint for movie table
 """
+from datetime import date, datetime
 import os
+from typing import Optional
 from flask import Blueprint, jsonify, request
+from pydantic import BaseModel
+from flask_pydantic import validate
 
 from blueprints.movie.service import (
     svc_delete,
@@ -16,31 +20,108 @@ from blueprints.movie.service import (
 )
 
 
+class MovieItem(BaseModel):
+    """Movie item model"""
+
+    title: str
+    description: str
+    movie_year: str
+    rating: float
+    runtime: float
+    votes: int
+    revenue: float
+    metascore: int
+    created_at: Optional[str]
+
+
+class MovieDataModel(BaseModel):
+    """Movie data model"""
+
+    movie_id: int
+    title: str
+    description: str
+    movie_year: date
+    rating: float
+    runtime: float
+    votes: int
+    revenue: float
+    metascore: int
+    created_at: Optional[str | datetime | date]
+
+
+class MessageModel(BaseModel):
+    """Message model"""
+
+    message: str
+
+
+class ResponseModel(BaseModel):
+    """Movie response model"""
+
+    status: str | int
+    data: list[MovieDataModel | MessageModel]
+
+
+class FieldValueModel(BaseModel):
+    """Movie Field and Value mmodel"""
+
+    field: str
+    value: str | int | date | float
+
+
+class SearchModel(BaseModel):
+    """Movie search model"""
+
+    fields: list[FieldValueModel]
+
+
+class ValueModel(BaseModel):
+    """Value model"""
+
+    value: str | float | int | date
+
+
+class InModel(BaseModel):
+    """In Search model"""
+
+    field: str
+    value: list[ValueModel]
+
+
+class PostModel(BaseModel):
+    """Movie Post model status"""
+
+    status: int | str
+
+
 version = os.getenv("VERSION")
 movie_blueprint = Blueprint("movie", __name__, url_prefix=version)
 
 
 @movie_blueprint.route("/movie/movies", methods=["GET"])
+@validate()
 def get_all_records():
     """
     A GET handler. Returns all records for customer.
     """
     result = svc_get()
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @movie_blueprint.route("/movie/<movie_id>", methods=["GET"])
+@validate()
 def get_by_id(movie_id: int):
     """
     A GET handler. Returns record by a given identifier.
     """
     result = svc_get_by_id(movie_id)
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @movie_blueprint.route("/movie/create", methods=["POST"])
+@validate(body=MovieItem)
 def post_movie():
     """
     A POST handler. Creates a new movie record
@@ -54,10 +135,11 @@ def post_movie():
     if status == 200:
         return jsonify(status=201)
 
-    return jsonify(status=status)
+    return PostModel(status=status)
 
 
 @movie_blueprint.route("/movie/<movie_id>", methods=["PUT"])
+@validate(body=MovieItem)
 def put_movie(movie_id: int):
     """
     A PUT handler.
@@ -73,6 +155,7 @@ def put_movie(movie_id: int):
 
 
 @movie_blueprint.route("/movie/<movie_id>", methods=["DELETE"])
+@validate()
 def delete_movie(movie_id: int):
     """
     A DELETE handler
@@ -81,10 +164,11 @@ def delete_movie(movie_id: int):
 
     result = svc_delete(movie_id)
 
-    return jsonify(status=result["status"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @movie_blueprint.route("/movie/exact", methods=["POST"])
+@validate(body=SearchModel)
 def search_exact():
     """
     EXACT Search
@@ -95,10 +179,11 @@ def search_exact():
     payload = request.get_json()
 
     result = svc_exact_search(payload)
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @movie_blueprint.route("/movie/like", methods=["POST"])
+@validate(body=SearchModel)
 def like_search():
     """
     LIKE SEARCH
@@ -108,10 +193,11 @@ def like_search():
     payload = request.get_json()
 
     result = svc_like_search(payload)
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @movie_blueprint.route("/movie/in", methods=["POST"])
+@validate(body=InModel)
 def in_search():
     """
     IN SEARCH
@@ -121,4 +207,4 @@ def in_search():
     payload = request.get_json()
 
     result = svc_in_search(payload)
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
