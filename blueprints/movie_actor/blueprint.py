c470d9f098c7b785ce54d989d26cc8cc938ffce1
Jeet Soni <jeet.soni0822@gmail.com>
2024-04-25 10:24:06 -0400

    added PUT aEXACT route

--- blueprints/movie_actor/blueprint.py
+++ blueprints/movie_actor/blueprint.py
@@ -1,7 +1,14 @@
 import os
 
 from flask import Blueprint, jsonify, request
-from blueprints.movie_actor.service import svc_delete, svc_get, svc_get_by_id, svc_post
+from blueprints.movie_actor.service import (
+    svc_delete,
+    svc_exact_search,
+    svc_get,
+    svc_get_by_id,
+    svc_post,
+    svc_put,
+)
 
 
 version = os.getenv("VERSION")
@@ -44,6 +51,19 @@ def post_record():
         return jsonify(status=201)
 
 
+@movie_actor_blueprint.route("/movie_actor/<movie_id>/<actor_id>", methods=["PUT"])
+def put_record(movie_id: int, actor_id: int):
+    """
+    Updates a record
+    """
+    pkeys = f"{movie_id}"
+    pkeys = f"{pkeys}, {actor_id}"
+
+    payload = request.get_json()
+    result = svc_put(pkeys, payload)
+    return jsonify(status=result["status"], data=result["data"])
+
+
 @movie_actor_blueprint.route("/movie_actor/<movie_id>/<actor_id>", methods=["DELETE"])
 def delete_movie(movie_id: int, actor_id: int):
     """
@@ -58,4 +78,15 @@ def delete_movie(movie_id: int, actor_id: int):
     return jsonify(status=result["status"], data=result["data"])
 
 
+@movie_actor_blueprint.route("/movie_genre/exact", methods=["POST"])
+def search_exact():
+    """
+    EXACT Search
+    Retrives all records from movies for exact value match
+    """
 
+    # request object
+    payload = request.get_json()
+
+    result = svc_exact_search(payload)
+    return jsonify(status=result["status"], data=result["data"])
--- blueprints/movie_actor/service.py
+++ blueprints/movie_actor/service.py
@@ -41,22 +41,34 @@ def svc_post(payload):
     return result
 
 
-# def svc_put(id, payload):
-#     """
-#     PUT service
-#     """
-#     movie_id = payload["movie_id"]
-#     actor_id = payload["actor_id"]
-#     created_at = payload["created_at"]
+def svc_put(ids_, payload):
+    """
+    PUT service
+    """
 
-#     sql = f"""UPDATE {SCHEMA_NAME}.{MOVIE_ACTOR} SET movie_id = %(movie_id)s,
-#         actor_id = %(actor_id)s, created_at = %(created_at)s 
-#         WHERE genre_id = %(genre_id)s
-#         RETURNING *;"""
-#     params = {"name": name, "created_at": created_at, "genre_id": id}
+    # splitting the ids
+    ids = ids_.split(",")
 
-#     result = do_query(sql, params)
-#     return result
+    # payload
+    movie_id = payload["movie_id"]
+    actor_id = payload["actor_id"]
+
+    # deletes the record
+    delete_stat_sql = f"""DELETE FROM {SCHEMA_NAME}.{MOVIE_ACTOR} 
+            WHERE movie_id = %(movie_id)s AND actor_id = %(actor_id)s
+            RETURNING *;"""
+    delete_sql_params = {"movie_id": ids[0], "actor_id": ids[1]}
+
+    insert_stat_sql = f"""INSERT INTO {SCHEMA_NAME}.{MOVIE_ACTOR}(movie_id, actor_id)
+                        VALUES(%s, %s) RETURNING *;"""
+    insert_sql_params = [movie_id, actor_id]
+
+    # executes the query to delete the record from the table
+    do_query(delete_stat_sql, delete_sql_params)
+
+    # inserts the new record to the table
+    result = do_query(insert_stat_sql, insert_sql_params)
+    return result
 
 
 def svc_delete(ids_):
@@ -72,6 +84,26 @@ def svc_delete(ids_):
             WHERE movie_id = %s AND actor_id = %s
             RETURNING *;"""
     params = [movie_id, actor_id]
+
+    result = do_query(sql, params)
+    return result
+
+
+def svc_exact_search(payload):
+    """
+    EXACT search service
+    """
+
+    search_condition = ""
+    search_fields = payload["fields"]
+
+    for idx, field_obj in enumerate(search_fields):
+        field = field_obj["field"]
+        value = field_obj["value"]
+        if idx == 0:
+            search_condition = f"{field} = '{value}'"
+    sql = f"SELECT * FROM {SCHEMA_NAME}.{MOVIE_ACTOR} WHERE {search_condition};"
+    params = {"field": field, "value": value}
 
     result = do_query(sql, params)
     return result
