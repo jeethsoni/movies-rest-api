4d157c15652057fd11ec0dfdb14636212f672f3a
Jeet Soni <jeet.soni0822@gmail.com>
2024-04-26 15:37:56 -0400

    fixed delete route ResponseModel error

--- blueprints/director/blueprint.py
+++ blueprints/director/blueprint.py
@@ -1,5 +1,9 @@
+from datetime import date, datetime
 import os
+from typing import Optional
 from flask import Blueprint, jsonify, request
+from pydantic import BaseModel
+from flask_pydantic import validate
 
 from blueprints.director.service import (
     svc_get,
@@ -13,31 +17,104 @@ from blueprints.director.service import (
 )
 
 
+class DirectorItems(BaseModel):
+    """
+     Items
+    """
+
+    first_name: str
+    last_name: str
+    created_at: Optional[str]
+
+
+class DirectorDataModel(BaseModel):
+    """
+     Data Model
+    """
+
+    director_id: int
+    first_name: str
+    last_name: str
+    created_at: Optional[str | datetime | date]
+
+
+class MessageModel(BaseModel):
+    """
+    Message model
+    """
+
+    message: str
+
+
+class ResponseModel(BaseModel):
+    """
+    Response model
+    """
+
+    status: int
+    data: list[DirectorDataModel | MessageModel]
+
+
+class FieldValueModel(BaseModel):
+    """
+    Field and value model
+    """
+
+    field: str
+    value: int | str | date | float
+
+
+class SearchModel(BaseModel):
+    """
+    Search model
+    """
+
+    fields: list[FieldValueModel]
+
+
+class ValueModel(BaseModel):
+    value: int | str | date | float
+
+
+class InModel(BaseModel):
+    field: str
+    values: list[ValueModel]
+
+
+class PostModel(BaseModel):
+    """Post model status"""
+
+    status: int | str
+
+
 version = os.getenv("VERSION")
 director_blueprint = Blueprint("director", __name__, url_prefix=version)
 
 
 @director_blueprint.route("/director/directors", methods=["GET"])
+@validate()
 def get_all_records():
     """
     A GET handler. Returns all records for director.
     """
     result = svc_get()
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @director_blueprint.route("/director/<director_id>", methods=["GET"])
+@validate()
 def get_by_id(director_id: int):
     """
     A GET handler. Returns record by a given identifier.
     """
     result = svc_get_by_id(director_id)
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @director_blueprint.route("/director/create", methods=["POST"])
+@validate(body=DirectorItems)
 def post_director():
     """
     A POST handler. Creates a new movie record
@@ -49,12 +126,13 @@ def post_director():
 
     status = result["status"]
     if status == 200:
-        return jsonify(status=201)
+        status = 201
 
-    return jsonify(status=status)
+    return PostModel(status=status)
 
 
 @director_blueprint.route("/director/<director_id>", methods=["PUT"])
+@validate(body=DirectorItems)
 def put_director(director_id: int):
     """
     A PUT handler for director table
@@ -64,20 +142,22 @@ def put_director(director_id: int):
 
     result = svc_put(payload, director_id)
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @director_blueprint.route("/director/<director_id>", methods=["DELETE"])
+@validate()
 def delete_director(director_id: int):
     """
     A DELETE handle
     """
 
     result = svc_delete(director_id)
-    return jsonify(status=result["status"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @director_blueprint.route("/director/exact", methods=["POST"])
+@validate(body=SearchModel)
 def search_by_exact():
     """
     Exact search
@@ -86,10 +166,11 @@ def search_by_exact():
     payload = request.get_json()
     result = svc_exact_search(payload)
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @director_blueprint.route("/director/like", methods=["POST"])
+@validate(body=SearchModel)
 def search_by_like():
     """
     Like Search
@@ -98,10 +179,11 @@ def search_by_like():
     payload = request.get_json()
     result = svc_like_search(payload)
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
 
 
 @director_blueprint.route("/director/in", methods=["POST"])
+@validate(body=InModel)
 def search_by_in():
     """
     In Search
@@ -110,4 +192,4 @@ def search_by_in():
     payload = request.get_json()
     result = svc_in_search(payload)
 
-    return jsonify(status=result["status"], data=result["data"])
+    return ResponseModel(status=result["status"], data=result["data"])
